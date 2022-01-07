300ce1c156561a3a58bed688e7cadcabccaa2114
Toni Isotalo <toni.isotalo@profitsoftware.com>
2022-01-07 19:50:20 +0200

    Joining in progress

--- app.js
+++ app.js
@@ -6,31 +6,50 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, { /* options */ });
 
-let randomId = () => {
-  return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
-}
+
+const reserved = {};
+const rooms = {};
 
 io.on("connection", (socket) => {
-  console.log("new connection")
+    socket.on("reserve", username => {
+        reserved[socket.id] = username;
+        socket.emit("reserved", socket.id);
+    })
+    socket.on("join", ({roomId, userId}) => {
+        if(userId in reserved) {
+            if(!(roomId in rooms)) {
+                rooms[roomId] = {
+                    "players": [],
+                    "ships": {}
+                }
+            } else {
+                rooms[roomId]["players"].push(socket.id);
+            }
+            socket.emit("joined", reserved[userId]);
+            delete reserved[userId];
+        }
+    })
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/src/html/index.html")
+    res.sendFile(__dirname + "/src/html/index.html")
 })
-app.get("/game/:room/:name", (req, res) => {
-  res.sendFile(__dirname + "/src/html/game.html")
+app.get("/game/:room/:id", (req, res) => {
+    if(req.params.id in reserved) {
+        res.sendFile(__dirname + "/src/html/game.html")
+    } else {
+        res.redirect("/");
+    }
 })
 app.get("/static/*", (req, res) => {
-  const split = req.originalUrl.split("static/")[1];
-  res.sendFile(__dirname + "/src/" + split);
+    const split = req.originalUrl.split("static/")[1];
+    res.sendFile(__dirname + "/src/" + split);
 })
 
 const randomIndex = (arr) => {
-  return Math.floor(Math.random() * arr.length);
+    return Math.floor(Math.random() * arr.length);
 }
 
 httpServer.listen(3000, () => {
-  console.log("Listening")
+    console.log("Listening")
 }); 
